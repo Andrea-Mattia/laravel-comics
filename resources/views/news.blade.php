e94af92a483d21e8ffa8bb504320da0c2a72a15f
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-06-08 19:18:20 +0200

    add news section

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,3 +1,15 @@
 @extends('layouts.main')
 
-@section('head-tite', 'DC COMICS | NEWS')
+@section('head-title', 'DC COMICS | NEWS')
+
+@section('content')
+    <section class="news container">
+        <h1>News section</h1>
+        <ul>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est architecto molestiae accusantium. Magni nihil, perspiciatis, et enim quidem eius soluta cupiditate voluptatibus ratione facere distinctio! Tempore obcaecati illum molestias repellat.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est architecto molestiae accusantium. Magni nihil, perspiciatis, et enim quidem eius soluta cupiditate voluptatibus ratione facere distinctio! Tempore obcaecati illum molestias repellat.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est architecto molestiae accusantium. Magni nihil, perspiciatis, et enim quidem eius soluta cupiditate voluptatibus ratione facere distinctio! Tempore obcaecati illum molestias repellat.</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est architecto molestiae accusantium. Magni nihil, perspiciatis, et enim quidem eius soluta cupiditate voluptatibus ratione facere distinctio! Tempore obcaecati illum molestias repellat.</li>
+        </ul>
+    </section>
+@endsection
